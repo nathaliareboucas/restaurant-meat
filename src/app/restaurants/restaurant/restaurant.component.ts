091b9d39af8a5e96ebfac2e0beb91fc18856becd
Nathalia <nathreboucas5@gmail.com>
2019-01-12 01:56:37 -0300

    criando componente restaurants e o service

---
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -0,0 +1,16 @@
+import { Restaurant } from './restaurant.model';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurant',
+  templateUrl: './restaurant.component.html'
+})
+export class RestaurantComponent implements OnInit {
+
+  @Input() restaurant: Restaurant;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
