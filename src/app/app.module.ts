abec08eeb789fa1a4086e38e4f53b6bea99b5c4e
Nathalia <nathreboucas5@gmail.com>
2019-01-15 21:35:38 -0300

    Implementando o carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
@@ -38,7 +39,7 @@ import { ReviewsComponent } from './reviews/reviews.component';
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
