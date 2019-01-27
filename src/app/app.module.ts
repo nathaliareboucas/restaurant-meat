cac119815306805fc0178767f2f604bf4a851d98
Nathalia <nathreboucas5@gmail.com>
2019-01-26 21:52:19 -0300

    Adicionando serviços a um módulo compartilhado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './reviews/reviews.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -39,8 +38,7 @@ import { CoreModule } from './core/core.module';
     HttpModule,
     FormsModule,
     ReactiveFormsModule,
-    SharedModule,
-    CoreModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
   providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
+import { OrderService } from './../order/order.service';
+import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantsService } from './../restaurants/restaurants.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RatingComponent } from './rating/rating.component';
 import { RadioComponent } from './radio/radio.component';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { InputComponent } from './input/input.component';
 
 @NgModule({
@@ -14,5 +17,10 @@ import { InputComponent } from './input/input.component';
   ]
 })
 export class SharedModule {
-
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [RestaurantsService, ShoppingCartService, OrderService]
+    };
+  }
 }
